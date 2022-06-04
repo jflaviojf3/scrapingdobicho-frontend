1bd16c4624ce6062279f509913f473943963b275
jflaviojf3 <jflaviojf3@hotmail.com>
2022-06-04 15:40:16 -0300

    Adicionado tabela de resultado

--- ResultadoTabela.js
+++ ResultadoTabela.js
@@ -2,21 +2,21 @@ import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function ResultadoTabela(props) {
-    const rEstacao = JSON.stringify(props);
-    console.log("Resultado da Estacao "+rEstacao)
-    
+    const rEstacao = Object.values(props)
+    //var tituloResul = rEstacao[0];
+    //console.log(rEstacao)
     
     
     return(
     <View>
-        <Text style={styles.tituloResult}>{rEstacao.cor}</Text>
-        <Text style={styles.Resulta}>1º - {rEstacao.COR}</Text>
-        <Text style={styles.Resulta}>2º - </Text>
-        <Text style={styles.Resulta}>3º - </Text>
-        <Text style={styles.Resulta}>4º - </Text>
-        <Text style={styles.Resulta}>5º - </Text>
-        <Text style={styles.Resulta}>6º - </Text>
-        <Text style={styles.Resulta}>7º - </Text>
+        <Text style={styles.tituloResult}></Text>
+        <Text style={styles.Resulta}>1º - {rEstacao.p1}</Text>
+        <Text style={styles.Resulta}>2º - {rEstacao.p2}</Text>
+        <Text style={styles.Resulta}>3º - {rEstacao.p3}</Text>
+        <Text style={styles.Resulta}>4º - {rEstacao.p4}</Text>
+        <Text style={styles.Resulta}>5º - {rEstacao.p5}</Text>
+        <Text style={styles.Resulta}>6º - {rEstacao.p6}</Text>
+        <Text style={styles.Resulta}>7º - {rEstacao.p7}</Text>
     </View>
     )
 }
