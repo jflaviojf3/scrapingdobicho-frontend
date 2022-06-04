380595694d61bb5d09f3256c9891211b908f6c3b
jflaviojf3 <jflaviojf3@hotmail.com>
2022-06-04 15:39:52 -0300

    teste com os resultados.

--- App.js
+++ App.js
@@ -11,9 +11,7 @@ async function getResultadoDoDia(url) {
 
   let estacoes = await selecionarResult
   let titulo = await selecionarResult.tituloResultado
-  console.log(estacoes)
-
-  console.log(typeof estacoes + "1")
+  //console.log(typeof  estacoes)
   return await estacoes
 }
 
@@ -21,63 +19,47 @@ async function getResultadoDoDia(url) {
 export default function App() {
 
   const [tituloJogo, setTituloJogo] = useState("Titulo Resultado");
-  const [resultado, setResultado] = useState({
-    "COR": {
-      "p1": "5469-18",
-      "p2": "5238-10",
-      "p3": "8003-1",
-      "p4": "9675-19",
-      "p5": "8905-2",
-      "p6": "7290-23",
-      "p7": "646-12",
-    },
-    "PT": {
-      "p1": "2647-12",
-      "p2": "0559-15",
-      "p3": "3866-17",
-      "p4": "2324-6",
-      "p5": "1159-15",
-      "p6": "0555-14",
-      "p7": "479-20",
-    },
-    "PTM": {
-      "p1": "0426-7",
-      "p2": "6406-2",
-      "p3": "0344-11",
-      "p4": "7304-1",
-      "p5": "3699-25",
-      "p6": "8179-20",
-      "p7": "728-7",
-    },
-    "PTN": {
-      "p1": "6787-22",
-      "p2": "2930-8",
-      "p3": "2518-5",
-      "p4": "4651-13",
-      "p5": "5341-11",
-      "p6": "2227-7",
-      "p7": "885-22",
-    },
-    "PTV": {
-      "p1": "2657-15",
-      "p2": "5217-5",
-      "p3": "5661-16",
-      "p4": "3585-22",
-      "p5": "5482-21",
-      "p6": "2602-1",
-      "p7": "861-16",
-    },
-    "tituloResultado": "Quinta-Feira, 02 de Junho de 2022",
-  })
+  const [resultado, setResultado] = useState([])
 
   const url = 'https://scrapingdobicho-backend.herokuapp.com/resultado-dia'
 
-  async function atualizaTitulo (){
-    let tituloRes = await getResultadoDoDia(url);
-    console.log(typeof  tituloRes.tituloResultado + "2")
-    setTituloJogo(tituloRes.tituloResultado)
+  async function atualizaTitulo() {
+    var resultadoDoDia = {
+      "tituloResultado": "Sexta-Feira, 04 de Junho de 2022",
+      "PTM": {
+        "p1": "5999-66",
+        "p2": "9613-4",
+        "p3": "7971-18",
+        "p4": "2686-22",
+        "p5": "3406-2",
+        "p6": "9675-19",
+        "p7": "668-17"
+      }
+    }
+
+    //await getResultadoDoDia(url);
+    var valuesObj = Object.values(resultadoDoDia)
+    var keysObj = Object.keys(resultadoDoDia)
+    setTituloJogo(valuesObj[0])
+    setResultado(valuesObj)
   }
 
+  var mapResult = () => {
+    return (
+      resultado.map(element => {
+        return (
+          <View key={element.key} style={{ margin: 10 }}>
+            <Text>1º - {element.p1}</Text>
+            <Text>2º - {element.p2}</Text>
+            <Text>3º - {element.p3}</Text>
+            <Text>4º - {element.p4}</Text>
+            <Text>5º - {element.p5}</Text>
+            <Text>6º - {element.p6}</Text>
+            <Text>7º - {element.p7}</Text>
+          </View>
+        );
+      }))
+  }
   useEffect(() => {
     //let tituloRes = await getResultadoDoDia(url);
     //setTituloJogo(tituloRes.tituloResultado)
@@ -87,13 +69,11 @@ export default function App() {
     <View style={styles.container}>
       <Text style={styles.titulo}>Resultado do Jogo do Bicho</Text>
       <Text style={styles.titulo}>{tituloJogo}</Text>
-      <ResultadoTabela resultadoEstacao = {resultado} />
+      <Text>{mapResult()}</Text>
       <TouchableOpacity
         onPress={() => atualizaTitulo()}
-        title={'Adicionar Data2'}
         style={styles.botaoTitulo}
-      >
-        <Text>{'Atualizar Data'}</Text>
+      ><Text>{'Atualizar Data'}</Text>
       </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
